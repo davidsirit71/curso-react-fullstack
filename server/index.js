d989f5aa09e335bca3747e732fb77535ad16a6fa
ds-global <david.sirit@globalincubator>
2019-10-03 12:05:58 +0200

    new route creation

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const app = express();   //llamado de express como una funcion
+
+app.get('/', (req, res) =>{
+    res.send({hi: 'there' });
+});
+
+app.listen(5000);
