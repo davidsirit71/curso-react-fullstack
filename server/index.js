2e4e3379a203f50d72b1fc50034842fc13d2bcfb
David Sirit <davidsirit@yahoo.com>
2019-10-05 18:20:48 +0200

    greetig changed

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();   //llamado de express como una funcion
 
 app.get('/', (req, res) =>{
-    res.send({hi: 'there' });
+    res.send({hi: 'there', by: "buddy" });
 });
 
 const PORT = process.env.PORT || 5000;
