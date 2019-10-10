1d09d557cede22fb73f366fe919aa6361553827f
David Sirit <davidsirit@yahoo.com>
2019-10-10 23:15:48 +0200

    add mongoose initial settings

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 // const passportConfig = require('./server/services/passport'); // mejor como sigue
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express(); //express como una funcion
 
