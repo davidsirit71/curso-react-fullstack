cba8469178d95dcefccda14b02ab787a3540690e
David Sirit <davidsirit@yahoo.com>
2019-10-11 20:52:53 +0200

    pequeña prueba

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -18,6 +18,7 @@ module.exports = app => {
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
+    // res.send(req.session); // esto es solo para probar que contine la sesion
   });
 
 
