5c233e34b7502eaa18c0929c180ac939f0c50f45
David Sirit <davidsirit@yahoo.com>
2019-10-11 19:22:55 +0200

    testing authentication

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport')
 const keys = require('./config/keys');
 require('./models/User'); // el orden de estas sentencias es importante
 require('./services/passport');  // const passportConfig = ... no es necesario, en este caso
@@ -7,6 +9,15 @@ require('./services/passport');  // const passportConfig = ... no es necesario, 
 mongoose.connect(keys.mongoURI);
 
 const app = express(); //express como una funcion
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 dias dura la cookie
+        keys: [keys.cookieKey] // encripta cualquier cookie, puden haber mas claves
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app); // hacer require retorna una funcion que luego llama a la funcion app
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,15 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users'); // esto es el Model Class
 
+passport.serializeUser((user, done) => {
+  done(null, user.id); // este es el Id que mongoDB crea para la coleccion
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user));
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,20 +22,20 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accesToken, refreshToken, profile, done) => {
-      User.findOne({googleId: profile.id})
-        .then((exitingUser) => {
-          if(exitingUser){
-            // we already have a record in DB
-            done(null, exitingUser); // todo bien el primer argumento es null
-          } else{
-            // we don't have a record with this ID, 
-            new User({
-              googleId: profile.id,
-              nameTEST: profile.name.familyName
-            }).save()
-              .then(user => done(null, user));
-          }
-        });
+      User.findOne({ googleId: profile.id }).then(exitingUser => {
+        if (exitingUser) {
+          // we already have a record in DB
+          done(null, exitingUser); // todo bien el primer argumento es null
+        } else {
+          // we don't have a record with this ID,
+          new User({
+            googleId: profile.id,
+            nameTEST: profile.name.familyName
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
